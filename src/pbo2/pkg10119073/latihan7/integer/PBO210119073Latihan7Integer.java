a8999e7f6d8a62ecca2c70e21cfdeff48dc53381
aghniadm <aghniadewii@gmail.com>
2020-10-18 18:44:10 +0700

    PBO2-10119073-Latihan7-Integer

---
+++ src/pbo2/pkg10119073/latihan7/integer/PBO210119073Latihan7Integer.java
@@ -0,0 +1,34 @@
+package pbo2.pkg10119073.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk mengimplementasikan
+ * integer
+ *
+ */
+public class PBO210119073Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte  : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int   : " + i);
+        System.out.println("long  : " + l);
+    }
+    
+}
